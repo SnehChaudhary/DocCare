79a9c2536d88cb6b91d866d2f41bb401c91c9ec3
SnehChaudhary <117535922+SnehChaudhary@users.noreply.github.com>
2024-02-07 23:20:51 +0530

    Added Signup Form

--- frontend/src/components/Hospitalsignup.js
+++ frontend/src/components/Hospitalsignup.js
@@ -2,9 +2,41 @@ import React from 'react'
 
 const Hospitalsignup = () => {
   return (
-    <div>
-      I am Hospital Signup.
-    </div>
+    <div className='container my-3 fw-bold'>
+    <form>
+      <div className="row">
+        <div className="mb-3 col-md-6">
+          <label for="exampleFormControlInput1" className="form-label">Name</label>
+          <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Name" />
+        </div>
+        <div className="mb-3 col-md-6">
+          <label for="exampleFormControlInput1" className="form-label">Address</label>
+          <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Address" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="mb-3 col-md-6">
+          <label for="exampleFormControlInput1" className="form-label">Secret Key</label>
+          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" />
+        </div>
+        <div className="mb-3 col-md-6">
+          <label for="exampleFormControlInput1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Password" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="mb-3 col-md-6">
+          <label for="exampleFormControlInput1" className="form-label">Contact</label>
+          <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Contact" />
+        </div>
+        <div className="mb-3 col-md-6">
+          <label for="exampleFormControlInput1" className="form-label">Total Patient</label>
+          <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Count" />
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
   )
 }
 
